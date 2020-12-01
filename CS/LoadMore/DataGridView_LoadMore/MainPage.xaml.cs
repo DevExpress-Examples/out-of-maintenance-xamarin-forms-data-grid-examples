722ee5b57143318572c96c1c56bdc61a0ac8f049
Elena Dvoryadkina <elena.dvoryadkina@devexpress.com>
2020-12-01 12:19:04 +0300

    Added the LoadMore example

---
+++ CS/LoadMore/DataGridView_LoadMore/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace DataGridView_LoadMore {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+    }
+}
--- Readme.md
+++ Readme.md
@@ -3,3 +3,4 @@ Examples:
 - [Getting Started](./CS/GettingStarted)  
 - [Grid Columns](./CS/Columns)
 - [Pull to Refresh](./CS/PullToRefresh)
+- [Load More](./CS/LoadMore)
