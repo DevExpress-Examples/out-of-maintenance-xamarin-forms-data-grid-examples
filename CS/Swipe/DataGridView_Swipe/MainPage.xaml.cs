72c65f297b30ac154c964c648df099ac904664ef
Elena Dvoryadkina <elena.dvoryadkina@devexpress.com>
2020-12-02 11:31:34 +0300

    Added the Swipe example

---
+++ CS/Swipe/DataGridView_Swipe/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using DevExpress.XamarinForms.DataGrid;
+
+namespace DataGridView_Swipe {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void Swipe_ShowCustomerInfo(object sender, SwipeItemTapEventArgs e) {
+            var customer = (e.Item as Order).Customer;
+            string customerName = customer.Name;
+            string customerPhone = customer.Phone;
+            DisplayAlert("Customer", "Name: " + customerName + "\n" + "Phone: " + customerPhone, "OK");
+        }
+        private void Swipe_Delete(object sender, SwipeItemTapEventArgs e) {
+            grid.DeleteRow(e.RowHandle);
+        }
+
+    }
+}
--- Readme.md
+++ Readme.md
@@ -4,3 +4,4 @@ Examples:
 - [Grid Columns](./CS/Columns)
 - [Pull to Refresh](./CS/PullToRefresh)
 - [Load More](./CS/LoadMore)
+- [Swipe Actions](./CS/Swipe)
